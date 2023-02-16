81fe16fd8955b7be434876eff27d609eda81b1d1
Sriram Teja Rella <rellasriram@gmail.com>
2023-02-16 18:25:59 -0500

    3-in-1 mockup

---
+++ BuzzPool_3_in_1/BuzzPool_3_in_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuzzPool_3_in_1
+//
+//  Created by Sriram Rella on 2/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
